12ad04989690743090644a8c2348226d3a2f4186
STAM <stam@epicm.org>
2015-10-13 14:35:33 +0300

    main form

--- Sources/PD.GUI/UI/FrmMain.Designer.cs
+++ Sources/PD.GUI/UI/FrmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace process_demonizator.UI
 {
-    partial class FrmMain
+    sealed partial class FrmMain
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,24 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.smenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.smenuSettions = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.smenuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this._notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile,
+            this.menuEdit,
+            this.menuHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(896, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuFile
+            // 
+            this.menuFile.BackColor = System.Drawing.SystemColors.Control;
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smenuExit});
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(37, 20);
+            this.menuFile.Text = "File";
+            // 
+            // smenuExit
+            // 
+            this.smenuExit.Name = "smenuExit";
+            this.smenuExit.Size = new System.Drawing.Size(152, 22);
+            this.smenuExit.Text = "Exit";
+            // 
+            // menuEdit
+            // 
+            this.menuEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smenuSettions});
+            this.menuEdit.Name = "menuEdit";
+            this.menuEdit.Size = new System.Drawing.Size(39, 20);
+            this.menuEdit.Text = "Edit";
+            // 
+            // smenuSettions
+            // 
+            this.smenuSettions.Name = "smenuSettions";
+            this.smenuSettions.Size = new System.Drawing.Size(152, 22);
+            this.smenuSettions.Text = "Settings";
+            // 
+            // menuHelp
+            // 
+            this.menuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smenuAbout});
+            this.menuHelp.Name = "menuHelp";
+            this.menuHelp.Size = new System.Drawing.Size(44, 20);
+            this.menuHelp.Text = "Help";
+            // 
+            // smenuAbout
+            // 
+            this.smenuAbout.Name = "smenuAbout";
+            this.smenuAbout.Size = new System.Drawing.Size(152, 22);
+            this.smenuAbout.Text = "About";
+            // 
+            // _notifyIcon
+            // 
+            this._notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("_notifyIcon.Icon")));
+            this._notifyIcon.Visible = true;
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(896, 515);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "_";
+            this.Text = "name + ver";
             this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuFile;
+        private System.Windows.Forms.ToolStripMenuItem smenuExit;
+        private System.Windows.Forms.ToolStripMenuItem menuEdit;
+        private System.Windows.Forms.ToolStripMenuItem smenuSettions;
+        private System.Windows.Forms.ToolStripMenuItem menuHelp;
+        private System.Windows.Forms.ToolStripMenuItem smenuAbout;
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
     }
 }
 
--- Sources/PD.GUI/UI/FrmMain.cs
+++ Sources/PD.GUI/UI/FrmMain.cs
@@ -1,13 +1,54 @@
 ﻿using System;
+using System.Drawing;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace process_demonizator.UI
 {
-    public partial class FrmMain : Form
+    public sealed partial class FrmMain : Form
     {
         public FrmMain()
         {
+            var progNameVer = AssemblyProduct + @" " + AssemblyVersion;
             InitializeComponent();
+            Text = progNameVer;
+            _notifyIcon.Text = progNameVer;
+            Resize += (FormForTray_Resize);
+            _notifyIcon.MouseClick += (_notifyIcon_MouseClick);
+        }
+
+        public string AssemblyVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+
+        public string AssemblyProduct
+        {
+            get
+            {
+                var attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                return attributes.Length == 0 ? "" : ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+              
+
+        private FormWindowState _oldFormState;
+        void FormForTray_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+            {
+                Hide();
+            }
+        }
+
+        void _notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            if (WindowState == FormWindowState.Normal || WindowState == FormWindowState.Maximized){
+                _oldFormState = WindowState;
+                WindowState = FormWindowState.Minimized;
+            }else{
+                Show();
+                WindowState = _oldFormState;
+            }
         }
 
         private void FrmMain_Load(object sender, EventArgs e)
